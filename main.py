55aae03eb6cbdc7b668684d1e09160f298c98ae0
irem <yasav07irem@hotmail.com>
2026-05-07 17:28:18 +0300

    feat: add pcap file parsing functionality and include a sample pcap file for testing

--- main.py
+++ main.py
@@ -10,13 +10,23 @@ def main():
         description="NetScanner: Nmap Entegrasyonlu PCAP Analiz ve Anomali Tespit Motoru"
     )
     
-    # İleride PCAP okuma ve Canlı dinleme argümanları buraya eklenecek
+    # PCAP okuma argümanı
+    parser.add_argument("-p", "--pcap", help="Analiz edilecek .pcap dosyasının yolu")
     parser.add_argument("-t", "--target", help="Hedef IP veya Domain adresi")
     parser.add_argument("--test-nmap", action="store_true", help="Nmap entegrasyonunu test et (Hızlı Tarama)")
     
     args = parser.parse_args()
 
-    if args.test_nmap and args.target:
+    if args.pcap:
+        print(f"[*] {args.pcap} dosyası okunuyor ve analiz ediliyor...")
+        from modules.packet_engine.pcap_reader import PcapReader
+        reader = PcapReader(args.pcap)
+        if reader.read_pcap():
+            paketler = reader.parse_packets()
+            print("\n[*] Parse Edilen İlk 5 Paket:")
+            for p in paketler[:5]:
+                print(p)
+    elif args.test_nmap and args.target:
         print(f"[*] {args.target} için entegrasyon testi başlatılıyor...")
         hiz = Ayarlar.TARAMA_HIZI
         nmap_calistir(["nmap", hiz, "-F", args.target], args.target, "test_tarama")
